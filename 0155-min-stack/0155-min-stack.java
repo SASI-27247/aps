1d838e1def5035755ea327cf1dc7cb54f9d3ccdb
SASI-27247 <vtu27247@veltech.edu.in>
2026-04-13 09:44:20 +0530

    Time: 5 ms (85.74%), Space: 47.2 MB (34.91%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,33 +1,39 @@
-import java.util.*;
+import java.util.Stack;
 
 class MinStack {
 
     private Stack<Integer> stack;
     private Stack<Integer> minStack;
 
+    // Initialize
     public MinStack() {
         stack = new Stack<>();
         minStack = new Stack<>();
     }
 
+    // Push element
     public void push(int val) {
         stack.push(val);
-        if (minStack.isEmpty()) {
+
+        if (minStack.isEmpty() || val <= minStack.peek()) {
             minStack.push(val);
         } else {
-            minStack.push(Math.min(val, minStack.peek()));
+            minStack.push(minStack.peek());
         }
     }
 
+    // Pop element
     public void pop() {
         stack.pop();
         minStack.pop();
     }
 
+    // Get top element
     public int top() {
         return stack.peek();
     }
 
+    // Get minimum element
     public int getMin() {
         return minStack.peek();
     }
